18709459d1dda4a982bc26c1ec1655b611d2f734
devaselviana <selvianadeva8@gmail.com>
2025-02-17 13:58:34 +0700

    cimiy

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'Hello World';
+ });
+ Route::get('/world', function () {
+    return 'World';
+ });
+ Route::get('/Selamat Datang', function () {
+    return 'Selamat Datang';
+ });
+ Route::get('/about', function () {
+   return 'Nim 2341760060 Nama Deva Selviana';
+});
+
+Route::get('/user/{name}', function ($name) {
+   return 'Nama Deva Selviana '.$name;
+   });
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+      return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+  });
+Route::get('/articles/{articles}/comments/{comment}', function ($postId, $commentId) {
+   return 'Halaman Artikel'.$postId." ID 5-: ".$commentId;
+});
+Route::get('/user/{name?}', function ($name=null) {
+   return 'Deva Selviana '.$name;
+   });
+Route::get('/user/{name?}', function ($name='Deva') {
+   return 'Deva Selviana '.$name;
+   });
+        
+   
